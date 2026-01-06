a946fc9dc18bc2e2b76ac3c046f23121a5c7bc78
Paul Karam <paulkaram1989@hotmail.com>
2026-01-06 15:17:30 +0100

    builder pattern

---
+++ CreationalPatterns/BuilderPattern/Builder.Example/Core/SchedulerOptionsBuilder.cs
@@ -0,0 +1,39 @@
+﻿namespace Builder.Example.Core;
+
+public class SchedulerOptionsBuilder(string cronExpression)
+{
+    private readonly SchedulerOptions _options = new(cronExpression);
+
+    public SchedulerOptions Build() => _options;
+
+    public SchedulerOptionsBuilder WithStartDate(DateTime startDate)
+    {
+        _options.StartDate = startDate;
+        return this;
+    }
+
+    public SchedulerOptionsBuilder WithEndDate(DateTime endDate)
+    {
+        _options.EndDate = endDate;
+        return this;
+    }
+
+    public SchedulerOptionsBuilder WithStartAndEndDate(DateTime startDate, DateTime endDate)
+    {
+        _options.StartDate = startDate;
+        _options.EndDate = endDate;
+        return this;
+    }
+
+    public SchedulerOptionsBuilder WithActionToExecute(Action action)
+    {
+        _options.ActionToExecute = action;
+        return this;
+    }
+
+    public SchedulerOptionsBuilder WithNumberOfTimesToExecute(int numberOfTimes)
+    {
+        _options.NumberOfTimesToExecute = numberOfTimes;
+        return this;
+    }
+}
